9dfeacdd035d2fe916b916c0ef4016e218a2c50a
ZaingDai <zaingdai@gmail.com>
2025-04-24 18:03:01 +0200

    [UPDATE] wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# check if DB_HOST is set
+if [ -z "$DB_HOST" ]; then
+  echo "DB_HOST is not set. Setting it to \"sensareal-database\"."
+  export DB_HOST="sensareal-database"
+fi
+
 echo "Waiting for database to be ready..."
 while ! mysqladmin ping -h "$DB_HOST" --silent; do
   sleep 1
